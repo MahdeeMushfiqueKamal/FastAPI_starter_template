fb233265b2f494f8032a99fdda403adaf259ec8f
MahdeeMushfiqueKamal <mmkdhrubo@gmail.com>
2024-03-21 22:33:12 +0600

    added a dummy post endpoint

--- app/routers/hello.py
+++ app/routers/hello.py
@@ -6,3 +6,11 @@ router = APIRouter()
 @router.get("/")
 def read_root():
     return {"success": True,"message": "Hello, World!"}
+
+
+@router.post("/")
+def read_root_post(data: dict):
+    return {
+        "success": True,
+        "data": data
+    }
